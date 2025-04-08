6aac4d91aae1d93d10a21447a487e686fe713918
Mablenn <marcos.broseta@gmail.com>
2025-04-08 21:44:45 +0200

    Update Test01FX.java

--- samples/Test01FX.java
+++ samples/Test01FX.java
@@ -1,1 +1,25 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 
+public class Test01FX extends Application {
+  @Override
+	    public void start(Stage primaryStage) {
+	        Label label = new Label("Test Ventana");
+	        StackPane root = new StackPane(label);
+	        Scene scene = new Scene(root, 400, 300);
+
+	        primaryStage.setTitle("Ventana JavaFX");
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+	    }
+	
+	// Lanza la aplicación
+	public static void main(String[] args) {
+		launch(args);
+  }
+}
