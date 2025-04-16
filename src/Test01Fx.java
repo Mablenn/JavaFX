b5b7760d925154a67cf9eb2e11d381b4b3a9848c
Mablenn <marcos.broseta@gmail.com>
2025-04-16 13:18:37 +0200

    Update Test01Fx.java

--- src/Test01Fx.java
+++ src/Test01Fx.java
@@ -8,7 +8,9 @@ import javafx.scene.layout.StackPane;
 
 public class Test01FX extends Application {
   @Override
-	    public void start(Stage primaryStage) {
+	//Este es el punto de inicio de la aplicación
+	//Aquí creamos y definimos la interfaz gráfica
+	public void start(Stage primaryStage) {
 	        Label label = new Label("Test Ventana");
 	        StackPane root = new StackPane(label);
 	        Scene scene = new Scene(root, 400, 300);
@@ -19,6 +21,7 @@ public class Test01FX extends Application {
 	    }
 	
 	// Lanza la aplicación
+	//El método estático launch(args) es el encargado de lanzar la aplicación
 	public static void main(String[] args) {
 		launch(args);
   }
