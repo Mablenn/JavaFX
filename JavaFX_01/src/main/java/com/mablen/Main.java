36794bbd5be94a2bca5e50126f6867c7ab55771a
Mablenn <marcos.broseta@gmail.com>
2025-10-07 13:19:29 +0200

    Update Main.java

--- JavaFX_01/src/main/java/com/mablen/Main.java
+++ JavaFX_01/src/main/java/com/mablen/Main.java
@@ -5,17 +5,18 @@ import javafx.stage.Stage;
 
 
 /*
-    La estructura básica de una aplicación JavaFX
+     La estructura básica de una aplicación JavaFX
     está compuesta por los siguientes elementos:
     
-    -Clase Application
-    -Método start(Stage stage)
-    -Clase Stage
-    -Clase Scene
+    -Clase Application: Punto de entrada de una aplicación JavaFX.
+    -Clase Stage: Es la ventana que mostrará todos los comoponentes de nuestra UI. La podemos considerar como la ventana nativa.
+    -Clase Scene: Contiene los componentes (nodos) de la ventana.
+    -Método start(Stage stage): Aquí es donde se construye el gráfico de escena.
+    -Método launch(args): Este método es el encargado de crear una nueva instancia de la clase Application.
 */
 
 public class Main extends Application {
-    //Sobreescribir método start()
+   
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Ventana JavaFX");
