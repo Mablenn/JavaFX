f095d0a0ea65a7e3f86be8b031a260775f309df3
Mablenn <marcos.broseta@gmail.com>
2025-11-06 16:16:56 +0100

    Fix typo in JavaFX application documentation

--- JavaFX_01/src/main/java/com/mablen/Main.java
+++ JavaFX_01/src/main/java/com/mablen/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
     está compuesta por los siguientes elementos:
     
     -Clase Application: Punto de entrada de una aplicación JavaFX.
-    -Clase Stage: Es la ventana que mostrará todos los comoponentes de nuestra UI. La podemos considerar como la ventana nativa.
+    -Clase Stage: Es la ventana que mostrará todos los componentes de nuestra UI. La podemos considerar como la ventana nativa.
     -Clase Scene: Contiene los componentes (nodos) de la ventana.
     -Método start(Stage stage): Aquí es donde se construye el gráfico de escena.
     -Método launch(args): Este método es el encargado de crear una nueva instancia de la clase Application.
